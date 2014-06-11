dd6ac3c9a411cb097ec687f53549183ffc36d787
JuiceGrape <bmentjot@gmail.com>
2014-06-11 10:54:45 +0200

    added right click function

--- src/main/java/com/juicegrape/biodynamics/items/ItemSpecialBonemeals.java
+++ src/main/java/com/juicegrape/biodynamics/items/ItemSpecialBonemeals.java
@@ -5,11 +5,13 @@ import java.util.List;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 
 import com.juicegrape.biodynamics.blocks.ModBlocks;
 import com.juicegrape.biodynamics.items.common.BioItem;
@@ -93,5 +95,7 @@ public class ItemSpecialBonemeals extends BioItem {
         return false;
     }
 	
-
+	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player) {
+		return itemStack;
+	}
 }
