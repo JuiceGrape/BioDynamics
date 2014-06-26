bbce2e5b0c5b869e1d66d73e24893b7b8a3705ce
JuiceGrape <bmentjot@gmail.com>
2014-06-26 16:30:58 +0200

    Removed useless code

--- src/main/java/com/juicegrape/biodynamics/tileentity/TileEntityCable.java
+++ src/main/java/com/juicegrape/biodynamics/tileentity/TileEntityCable.java
@@ -9,42 +9,10 @@ import com.juicegrape.biodynamics.tileentity.common.TileEntityBattery;
 
 public class TileEntityCable extends TileEntityBattery  {
 	
-//	private List<ForgeDirection> receive;
-//	private int timer;
 
 	public TileEntityCable() {
 		super(800, 100, 100);
-	//	receive = new ArrayList<ForgeDirection>();
-	//	for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
-	//		receive.add(dir);
-	//	}
-	//	timer = 0;
 	}
-	
-	
-/*	@Override
-	public int receiveEnergy(ForgeDirection from, int maxReceive, boolean simulate) {
-		if (receive.contains(from))
-			receive.remove(from);
-		return super.receiveEnergy(from, maxReceive, simulate);
-	}
-	
-	@Override
-	protected ForgeDirection[] getOutputDirections() {
-		return receive.toArray(new ForgeDirection[]{null});
-	}
-	
-/*	@Override
-	public void updateEntity() {
-		timer++;
-		if (timer >= 20) {
-			for (ForgeDirection dir : ForgeDirection.VALID_DIRECTIONS) {
-				receive.add(dir);
-			}
-			timer = 0;
-		}
-		super.updateEntity();
-	} */
 	
 //TODO: make it not send power to where it received it from
 }
