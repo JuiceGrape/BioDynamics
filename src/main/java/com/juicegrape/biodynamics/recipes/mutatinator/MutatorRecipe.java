a64f968f736d8e52c04a8d3d7ad343c3b7f842e3
JuiceGrape <bmentjot@gmail.com>
2014-09-02 12:39:14 +0200

    Worked on mutator recipe

---
+++ src/main/java/com/juicegrape/biodynamics/recipes/mutatinator/MutatorRecipe.java
@@ -0,0 +1,25 @@
+package com.juicegrape.biodynamics.recipes.mutatinator;
+
+import net.minecraft.item.ItemStack;
+
+public class MutatorRecipe {
+	
+	ItemStack mainInput;
+	ItemStack[] otherInputs;
+	ItemStack output;
+	
+	public MutatorRecipe(ItemStack output, ItemStack main, ItemStack... others) {
+		mainInput = main;
+		otherInputs = others;
+		this.output = output;
+	}
+	
+	public int getOtherMaterialAmounts() {return otherInputs.length;}
+	
+	public boolean hasOtherInputs() {return getOtherMaterialAmounts() > 0;}
+	
+	public ItemStack getOutput() {return output;}
+
+	
+	
+}
